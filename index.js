98718095101ed29fb5b7774c825e1c1067a8c6a8
KUNALLATTA29 <kunallatta2@gmail.com>
2024-11-21 00:03:35 +0530

    first commit

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
 const express = require('express')
-const path = require('path')
+const users = require('./MOCK_DATA.json')
+const fs = require('fs')
 
 const app = express()
-
-app.use(express.json())
+const port  = 8000
 app.use(express.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname,'public')))
-app.set('view engine', 'ejs')
 
-app.get("/",function(req,res){
-    res.render("index")
+app.get('/api/users',(req,res)=>{
+    return res.json(users)
 })
 
-app.get('/profile/:username',function(req,res){
-    res.send(req.params.username)
+app.get('/users',(req,res)=>{
+    const html = `
+    ${users.map((users)=>`<li>${users.first_name}</li>`).join("")}
+    
+    `
+    return res.send(html)
 })
 
-app.get('/author/:username/:age', function(req,res){
-    res.send(`welcome ${req.params.username}, age ${req.params.age}`)
+app.get('/api/users/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const user = users.find(user => user.id === id);
+    return res.json(user)
 })
 
-app.listen(3000,function(){
-    console.log("server is running")
+app.post('/api/users',(req,res)=>{
+    const body = req.body
+    users.push({...body, id: users.length +1});
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users), (err,data)=>{
+        return res.json({status:"done"})
+    })
+    
 })
+
+app.put('/api/users/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    const body = req.body
+    const putuser = users.findIndex(user => user.id === id)
+
+    users[putuser] = {...users[putuser],...body}
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users), (err,data)=>{
+        return res.json({status:"done"})
+    })
+    
+})
+
+app.delete('/api/users/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    const deluser = users.findIndex(user=>user.id === id)
+
+    users.splice(deluser,1)
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users), (err,data)=>{
+        return res.json({status:"done"})
+    })
+})
+
+app.listen(port,()=>console.log("server is running on port: ",port))
